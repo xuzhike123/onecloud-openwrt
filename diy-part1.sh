c7b11245a5f409f74bce1e8c505cd56b358c3dc0
xuzhike123 <46637868+xuzhike123@users.noreply.github.com>
2026-08-09 20:39:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 取消注释或清理原有的 passwall 软件源，防止重复
+sed -i '/passwall/d' feeds.conf.default
 
-# Add a feed source
-sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
-
-#sed -i '2s/^#//' feeds.conf.default
-#sed -i '3s/^/#/' feeds.conf.default
+# 重新追加 Clean 的 PassWall 软件源
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall url?id=1xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
