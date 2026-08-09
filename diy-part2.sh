96e880be47ba59cb4e1c46585e0307ab1c247924
xuzhike123 <46637868+xuzhike123@users.noreply.github.com>
2026-08-09 19:39:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# 1. 替换 Golang 编译工具链为 23.x 兼容版本
+# 1. 替换 Golang 编译工具链为 23.x 兼容版本 (Passwall 必须)
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
-# 2. 彻底移除 Python 与 蓝牙等耗时且易报错的组件
+# 2. 清理容易报错及冲突的源码包
 rm -rf feeds/packages/lang/python
 rm -rf feeds/packages/utils/bluez
 rm -rf package/lean/luci-app-passwall
 
-# 3. 添加 xiaorouji 的 Passwall 专属软件源（自动完备依赖链）
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+# 3. 直接 Clone 适配 Lean 源码的最新 PassWall 及依赖包
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 
-# 4. 修改默认后台 IP 为 192.168.10.99
-sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
+# 4. 修改默认后台登录 IP 为 192.168.1.99
+sed -i 's/192.168.1.1/192.168.1.99/g' package/base-files/files/bin/config_generate
