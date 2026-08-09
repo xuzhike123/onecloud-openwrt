28dad3bb575a54d636fa143c60689329944f1396
xuzhike123 <46637868+xuzhike123@users.noreply.github.com>
2026-08-09 19:31:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,15 +4,14 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
-# 2. 彻底从源头删除 Python 语言环境包，防止被其他插件隐式依赖触发长达 1.5 小时的编译
+# 2. 彻底移除 Python 与 蓝牙等耗时且易报错的组件
 rm -rf feeds/packages/lang/python
-
-# 3. 清理 Lean 源码自带的旧版冲突 PassWall 及蓝牙包
 rm -rf feeds/packages/utils/bluez
 rm -rf package/lean/luci-app-passwall
 
-# 4. 引入官方 PassWall 依赖包
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
+# 3. 添加 xiaorouji 的 Passwall 专属软件源（自动完备依赖链）
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 
-# 5. 修改默认后台 IP 为 192.168.10.99
+# 4. 修改默认后台 IP 为 192.168.10.99
 sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
