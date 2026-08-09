f4e51914c639fd5bb4d48d9389e55ac56ac46ffc
xuzhike123 <46637868+xuzhike123@users.noreply.github.com>
2026-08-09 21:44:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# 1. 修改默认后台登录 IP 为 192.168.1.99
-sed -i 's/192.168.1.1/192.168.1.99/g' package/base-files/files/bin/config_generate
+# 1. 修改默认后台登录 IP 为 192.168.10.99
+sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
 
-# 2. 升级 Golang 编译工具链为 23.x 兼容版本（PassWall 编译必需）
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-# 3. 清理主线 Lean 源码自带的旧版冲突包与蓝牙无用包
+# 2. 清理 Lean 源码自带的旧版 PassWall 及冲突蓝牙模块
 rm -rf package/lean/luci-app-passwall
 rm -rf feeds/packages/utils/bluez
 
-# 4. 使用公有可访问的 PassWall 官方地址（添加 .git 后缀防止鉴权误判）
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+# 3. 替换 Golang 编译工具链为兼容版本 (SSR-Plus 编译必需)
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang.git -b 23.x feeds/packages/lang/golang
