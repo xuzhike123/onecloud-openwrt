95ca2c432d039bd95a25daa12751040d9c8455bf
xuzhike123 <46637868+xuzhike123@users.noreply.github.com>
2026-08-09 20:06:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# 1. 修改默认 IP
+# 1. 修改默认登录后台 IP 为 192.168.10.99
 sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
 
-# 2. 移除旧的依赖/冲突包
+# 2. 升级 Golang 编译工具链为兼容版本
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
+# 3. 清理 Lean 源码自带的旧版 passwall 冲突
 rm -rf package/lean/luci-app-passwall
+rm -rf feeds/packages/utils/bluez
 
-# 3. 重新克隆 PassWall 及其依赖包
+# 4. 直接拉取 Passwall 完整源码包到 package 目录（无需追加 feeds.conf.default）
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
