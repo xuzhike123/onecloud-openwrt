ae05871484452cee978b3fbd62048d78d7b1d327
xuzhike123 <46637868+xuzhike123@users.noreply.github.com>
2026-08-09 21:15:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-# 1. 自动删除 feeds.conf.default 中可能存在的重复 passwall 行
-sed -i '/passwall/d' feeds.conf.default
-
-# 2. 重新追加干净的源定义
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+# 不对 feeds.conf.default 做任何写入，防止源冲突
