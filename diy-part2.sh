42be5d9f2be2a707dd6f91996136aa7972afe15a
xuzhike123 <46637868+xuzhike123@users.noreply.github.com>
2026-08-10 22:42:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# 1. 修改默认后台登录 IP 为 192.168.1.99
-sed -i 's/192.168.1.1/192.168.1.99/g' package/base-files/files/bin/config_generate
+# 1. 修改默认后台登录 IP 为 192.168.10.99
+sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
 
-# 2. 清理 Lean 源码自带的冲突/旧版软件包
+# 2. 清理 Lean 源码自带的冲突包及无用蓝牙模块
 rm -rf package/lean/luci-app-passwall
 rm -rf feeds/packages/utils/bluez
-
-# 3. 升级 Golang 工具链为稳定版 (SSR-Plus 核心依赖)
-rm -rf feeds/packages/lang/golang
-git clone url?id=1sbwml/packages_lang_golang.git -b 23.x feeds/packages/lang/golang
